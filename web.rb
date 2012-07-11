b2fbfffeb0e93cf5a0d4100b94b9060e711e78a5
Jesse B. Hannah <jesse@jbhannah.net>
2012-07-11 16:33:28 -0700

    Build API request URI

--- web.rb
+++ web.rb
@@ -1,8 +1,23 @@
 require 'rubygems'
 require 'sinatra/base'
 
+require 'net/http'
+
 class Web < Sinatra::Base
   get '/' do
     "Hello world!"
+  end
+
+  get '/:region/:realm/:char' do
+    q = {
+      region: params[:region],
+      realm:  params[:realm],
+      char:   params[:char]
+    }
+
+    uri = URI('http://www.best-signatures.com/api/')
+    uri.query = URI.encode_www_form(q)
+
+    uri.to_s
   end
 end
