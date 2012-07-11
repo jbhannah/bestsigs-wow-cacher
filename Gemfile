d2116ea64f6d8104bcdb3ef94c6457788dea7cf1
Jesse B. Hannah <jesse@jbhannah.net>
2012-07-11 16:57:42 -0700

    DataMapper

--- Gemfile
+++ Gemfile
@@ -3,3 +3,13 @@ source :rubygems
 gem 'sinatra'
 gem 'foreman'
 gem 'thin'
+
+gem 'data_mapper'
+
+group :production do
+  gem 'dm-postgres-adapter'
+end
+
+group :development do
+  gem 'dm-sqlite-adapter'
+end
--- web.rb
+++ web.rb
@@ -2,8 +2,25 @@ require 'rubygems'
 require 'sinatra/base'
 
 require 'net/http'
+require 'data_mapper'
+
+class Character
+  include DataMapper::Resource
+
+  property :id,         Serial
+  property :region,     String
+  property :realm,      String
+  property :character,  String
+  property :created_at, DateTime
+  property :updated_at, DateTime
+end
 
 class Web < Sinatra::Base
+  configure do
+    DataMapper.setup(:default, (ENV["DATABASE_URL"] || "sqlite3:///#{Dir.pwd}/development.sqlite3"))
+    DataMapper.auto_upgrade!
+  end
+
   get '/' do
     "Hello world!"
   end
