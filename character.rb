1fa388f8f8a73d6bcd45d8ab14533731fecd0fa9
Jesse B. Hannah <jesse@jbhannah.net>
2012-07-11 17:30:18 -0700

    Character#set_api_uri

--- character.rb
+++ character.rb
@@ -3,6 +3,8 @@ require 'net/http'
 class Character
   include DataMapper::Resource
 
+  before :create, :set_api_uri
+
   property :id,         Serial
   property :region,     String
   property :realm,      String
@@ -12,10 +14,17 @@ class Character
   property :region,     String, required: true
   property :realm,      String, required: true
   property :char,       String, required: true
+  property :api_uri,    URI
+  property :img_uri,    URI
   property :created_at, DateTime
   property :updated_at, DateTime
 
   def fetch_img
+    api_uri.to_s
+  end
+
+  private
+  def set_api_uri
     q = {
       region: region,
       realm:  realm,
@@ -25,6 +34,6 @@ class Character
     uri = URI('http://www.best-signatures.com/api/')
     uri.query = URI.encode_www_form(q)
 
-    uri.to_s
+    self.api_uri = uri
   end
 end
